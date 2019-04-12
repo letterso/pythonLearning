a1a4e7c8c9000b2efd48b6525ab77ee4ece70659
letter <lettersony00@gmail.com>
2019-04-12 18:11:07 +0800

    modify 2048

--- project/2048/2048.py
+++ project/2048/2048.py
@@ -13,6 +13,7 @@ letters_code = [ord(ch) for ch in 'wsadrqWSADRQ']
 actions_dict = dict(zip(letters_code,actions*2))
 
 # 辅助函数
+# 监测键盘输入
 def get_user_action(keyboard):
 	# 获取有限输入，若输入无效堵塞程序
 	char = 'N'
@@ -20,18 +21,58 @@ def get_user_action(keyboard):
 		char = keyboard.getch()
 	return actions_dict[char]
 
-def 
+# 矩阵转置
+def transpose(field):
+	return [list(row) for row in zip(*field)]
+
+# 矩阵逆转
+def invert(field):
+	return [row[::-1] for row in field]
 
 # 棋盘定义
-
+class GameField(object):
+	def __init__(self,height = 4,width = 4,win = 2048):
+		self.height = height
+		self.width = width
+		self.win_score = win
+		self.score = 0
+		self.high_score = 0
+		self.reset()
+		
+	def.reset(self):
+		if self.score > self.high_score:
+			self.high_score = self.score
+		self.score = 0
+		self.field = [[0 for i in range(self.width)] for j in range(self.height)]
+		self.spawn()
+		self.spawn()
+	
+	def 
+		
 
 # 状态机实现
-def main():
+def main(stdscr):
 	def init():
 		game_field.resize()
 		return 'game'
 	
-def game():
+	def not_game(state):
+		
+	
+	def game():
+		
+	state_actions = {
+		'Init':init,
+		'Win':lambda:not_game('win'),
+		'GameOver':lambda:not_game('GameOver'),
+		'Game':game
+	}
+	
+	state = 'Init'
+	while state != 'Exit':
+		state = state_actions[state]()
+		
+	
 	
 		
 		
